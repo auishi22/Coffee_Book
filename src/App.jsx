4dde6770edde60bd2b9697ace68c2d648e150383
auishi22 <auishee222@gmail.com>
2025-05-31 10:40:22 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      
+      <h1 className='text-xl text-red-700 bg-blue-200 font-bold underline'>Vite + React ddddccccdcdxzczcccccd</h1>
+      
+    </>
+  )
+}
+
+export default App
